e6c1c66c6e7957f890c5a1922b2ec5f1179bf4ef
Aditya Kumar <121538037+itsadityakr@users.noreply.github.com>
2024-02-29 22:58:36 +0530

    Added Codes

---
+++ Codes/_1_Start_Coding.cpp
@@ -0,0 +1,50 @@
+/*__________________________PROBLEM__________________________*/
+/*
+Start Coding
+
+When learning a new language, we first learn to output some message. Here, we'll start with the famous Hello World message. Now, here you are given a function to complete. Don't worry about the ins and outs of functions, just add the command (cout<<"Hello World") to print Hello World.
+
+Input Format :
+No input
+
+Output format :
+Hello World
+Your Task:
+Your task is to complete the function below to print Hello World.
+
+Explanation:
+Hello World is printed.
+*/
+
+/*__________________________PROGRAM__________________________*/
+
+// { Driver Code Starts
+// Initial Template for C++
+
+#include<iostream>  // Header file for input/output operations
+
+using namespace std;
+
+void printHello();  // Function declaration
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+// Function to print "Hello World"
+void printHello(){
+    
+    cout << "Hello World";  // Printing "Hello World" to the console
+    
+}
+
+// { Driver Code Starts
+
+int main()
+{
+    // Calling function to print Hello World
+    printHello();  // Function call to print "Hello World"
+
+    return 0;  // Returning 0 to indicate successful execution
+}
+// } Driver Code Ends
